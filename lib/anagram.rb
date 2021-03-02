844c1ff55631a56fb8eca158034663bfbcc25cb5
Andrés Nuñez <andresanunez98@gmail.com>
2021-03-02 17:49:20 -0500

    commit tree c9273b72d4aea47f9eb87eecf65e5851bcbb5825

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :anagram
+
+def initialize(word)
+    @anagram = word
+    
+end
+
+def match(words)
+
+    words.select do |word|
+        word.split("").sort == @anagram.split("").sort
+
+    end
+    
+end
+
+
+end
+
+
